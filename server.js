5f531f10fa2ec03ed23041e8e6324b6d762c3f76
glennin-codes <glen.nyandiri@students.co.ke>
2022-09-30 10:11:52 +0300

    exporting files

--- server.js
+++ server.js
@@ -6,12 +6,22 @@ console.log("hello world");
 //uses common modules instead of es6 modules
 const Os = require('os');
 const path= require('path')
+const {add,subtract,multiply,divide}=require('./math');
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3));
 console.log(Os.type());
 console.log(Os.version());
 console.log(Os.homedir());
  
 // console.log(__dirname);
 // console.log(__filename);
-console.log(path.basename(__filename));
-console.log(path.dirname(__filename));
-console.log(path.extname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.dirname(__filename));
+// console.log(path.extname(__filename));
+// //using path.parse brings out the whole pathinform of an object description
+//  console.log(path.parse(__filename));
+
+
+ 
